c72db9afd0d7dab2e208053a89599b44e2c7004a
Matheus Barros <matheuslbarros@gmail.com>
2019-02-16 20:42:51 -0200

    Add env configuration

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 
 module.exports = {
   server: {
-    host: '127.0.0.1',
-    port: 9000,
+    host: process.env.HOST || '127.0.0.1',
+    port: process.env.PORT || 9000,
   },
   token: {
-    secret_key: '84ee498d-6b84-468c-b4c6-f0af7408303c',
+    secret_key: process.env.SECRET_KEY || '84ee498d-6b84-468c-b4c6-f0af7408303c',
   },
 };
