b3bccf66a6a90b8ee66d6ef98d22a3351e676c75
Matheus Barros <matheuslbarros@gmail.com>
2019-02-17 09:21:08 -0300

    Fix bcrypt issue

--- src/helpers/crypt.js
+++ src/helpers/crypt.js
@@ -1,11 +1,11 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const hash = (data) => {
   return bcrypt.hashSync(data, bcrypt.genSaltSync(1));
 };
 
 const compare = (data, encrypted) => {
-  return bcrypt.compare(data, encrypted);
+  return bcrypt.compareSync(data, encrypted);
 };
 
 module.exports = {
