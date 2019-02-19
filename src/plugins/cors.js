3ab5205cff0c89e3000cb89006ed8752f7b16886
Matheus Barros <matheuslbarros@gmail.com>
2019-02-18 21:19:07 -0300

    Fix cors allowed headers

--- src/plugins/cors.js
+++ src/plugins/cors.js
@@ -4,8 +4,8 @@ const options = {
   origins: ['*'],
   allowCredentials: 'true',
   exposeHeaders: ['content-type', 'content-length'],
-  methods: ['HEAD, OPTIONS, GET, POST, PUT, PATCH, DELETE'],
-  headers: ['Accept', 'Content-Type', 'Authorization'],
+  methods: ['HEAD', 'OPTIONS', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  headers: ['accept', 'content-type', 'authorization', 'x-access-token'],
 };
 
 module.exports = {
